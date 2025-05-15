5f7b50714963a2f573e1a87f47436df8dd6f3382
MUHAMMED SHAFOOK KELOTH <muhammed.shafook.keloth@redyoi.ai>
2025-05-15 16:54:12 +0400

    11: Implemented the test cases

--- catalog-service/src/test/java/com/moshfk/catalog_service/domain/ProductRepositoryTest.java
+++ catalog-service/src/test/java/com/moshfk/catalog_service/domain/ProductRepositoryTest.java
@@ -2,7 +2,10 @@ package com.moshfk.catalog_service.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
 import java.util.List;
+
+import org.assertj.core.api.BigDecimalAssert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -19,5 +22,19 @@ class ProductRepositoryTest {
     void testFindAllProducts_Success() {
         List<Product> products = productRepository.findAll();
         assertThat(products).hasSize(15);
+    }
+
+    @Test
+    void testFindProductByCode_Success() {
+        Product product = productRepository.findByCode("P100").orElseThrow();
+        assertThat(product.getCode()).isEqualTo("P100");
+        assertThat(product.getName()).isEqualTo("The Hunger Games");
+        assertThat(product.getDescription()).isEqualTo("Winning will make you famous, losing means certain death.");
+        assertThat(product.getPrice()).isEqualTo(new BigDecimal("34.00"));
+    }
+
+    @Test
+    void testFindProductByInvalidCode_Failure() {
+        assertThat(productRepository.findByCode("invalid_code")).isEmpty();
     }
 }
